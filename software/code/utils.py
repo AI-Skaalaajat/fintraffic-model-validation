3ef31c89c2438492dedb78a9aef098f893ca51de
JamiHam <jami.hamala@gmail.com>
2025-12-12 09:18:30 +0200

    Created plot_loss_curves() function

--- software/code/utils.py
+++ software/code/utils.py
@@ -1,6 +1,8 @@
 import torch
 from pathlib import Path
 import torch.nn as nn
+import matplotlib.pyplot as plt
+from typing import Dict, List
 
 def save_model(model: nn.Module,
                directory: Path,
@@ -9,3 +11,16 @@ def save_model(model: nn.Module,
     model_path = directory / model_name
     torch.save(obj=model.state_dict(),
                f=model_path)
+    
+def plot_loss_curves(results: Dict[str, List[float]]):
+    train_loss = results['train_loss']
+    validation_loss = results['validation_loss']
+    epochs = range(len(results['train_loss']))
+
+    plt.figure(figsize=(15, 7))
+    plt.plot(epochs, train_loss, label='train_loss')
+    plt.plot(epochs, validation_loss, label='validation_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+    plt.show()
