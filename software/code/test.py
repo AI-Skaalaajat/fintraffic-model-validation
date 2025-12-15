e20594e891cea962e1da10bd346ff2203b86bbf7
JamiHam <jami.hamala@gmail.com>
2025-12-15 14:37:27 +0200

    Created test.py

---
+++ software/code/test.py
@@ -0,0 +1,45 @@
+import torch
+from torchvision import datasets
+from matplotlib import pyplot as plt
+from tqdm.auto import tqdm
+from pathlib import Path
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import model_setup
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+data_path = Path("software/data")
+model_path = Path("software/models")
+test_directory = data_path / "test"
+model_name = "ResNet-50.pth"
+
+class_names = datasets.ImageFolder(test_directory).classes
+
+model, preprocess = model_setup.setup_resnet(layers=50,
+                                             pretrained=False,
+                                             class_names=class_names,
+                                             device=device)
+
+model.load_state_dict(torch.load(f=model_path / model_name))
+
+test_set = datasets.ImageFolder(test_directory, transform=preprocess)
+test_dataloader = torch.utils.data.DataLoader(test_set)
+
+model.eval()
+
+labels = []
+predicted_labels = []
+
+with torch.inference_mode():
+    for input, label in tqdm(test_dataloader):
+        input, label = input.to(device), label.to(device)
+
+        output = model(input)
+        predicted_label = torch.argmax(torch.softmax(output, dim=1), dim=1)
+
+        labels.append(label.item())
+        predicted_labels.append(predicted_label.cpu())
+
+    confusion_matrix = confusion_matrix(labels, predicted_labels)
+    disp = ConfusionMatrixDisplay(confusion_matrix, display_labels=class_names)
+    disp.plot()
+    plt.show()
