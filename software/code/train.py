265d2b65d555c75ae5457f107d4b54ccb848ccc0
JamiHam <jami.hamala@gmail.com>
2026-01-06 14:18:35 +0200

    Changed train.py to use setup() function

--- software/code/train.py
+++ software/code/train.py
@@ -16,10 +16,13 @@ num_workers = os.cpu_count()
 class_names = datasets.ImageFolder(train_directory).classes
 
 def main():
-    model, preprocess = model_setup.setup_resnet(layers=50,
-                                                 pretrained=True,
-                                                 class_names=class_names,
-                                                 device=device)
+    model, preprocess = model_setup.setup(model=config['model'],
+                                          pretrained=config['pretrained'],
+                                          class_names=class_names,
+                                          device=device,
+                                          size=config['size'],
+                                          resnet_layers=config['resnet_layers'],
+                                          swin_transformer_version=config['swin_transformer_version'])
 
     dataset = datasets.ImageFolder(train_directory, transform=preprocess)
     train_set, validation_set = torch.utils.data.random_split(dataset, [0.8, 0.2])
@@ -48,7 +51,7 @@ def main():
                            device=device)
 
     utils.plot_loss_curves(results)
-    utils.save_model(model, model_directory, config['output_file_name'])
+    utils.save_model(model, model_directory, config['model_file_name'])
 
 if __name__ == '__main__':
     main()
