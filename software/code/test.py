4c1d014e77e6ebdc66a6d54a02fab62f887264e1
JamiHam <jami.hamala@gmail.com>
2026-01-06 14:23:29 +0200

    test.py now uses the config file

--- software/code/test.py
+++ software/code/test.py
@@ -5,22 +5,26 @@ from tqdm.auto import tqdm
 from pathlib import Path
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
 import model_setup
+from utils import read_config
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-data_path = Path("software/data")
-model_path = Path("software/models")
-test_directory = data_path / "test"
-model_name = "ResNet-50.pth"
+config = read_config()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+test_directory = Path(config['test_directory'])
+model_directory = Path(config['model_directory'])
+model_name = config['model_file_name']
 
 def main():
     class_names = datasets.ImageFolder(test_directory).classes
 
-    model, preprocess = model_setup.setup_resnet(layers=50,
-                                                pretrained=False,
-                                                class_names=class_names,
-                                                device=device)
+    model, preprocess = model_setup.setup(model=config['model'],
+                                          pretrained=False,
+                                          class_names=class_names,
+                                          device=device,
+                                          size=config['size'],
+                                          resnet_layers=config['resnet_layers'],
+                                          swin_transformer_version=config['swin_transformer_version'])
 
-    model.load_state_dict(torch.load(f=model_path / model_name))
+    model.load_state_dict(torch.load(f=model_directory / model_name))
 
     test_set = datasets.ImageFolder(test_directory, transform=preprocess)
     test_dataloader = torch.utils.data.DataLoader(test_set)
@@ -49,20 +53,20 @@ def main():
         recall = recall_score(true_labels, predicted_labels, labels=label_indexes, average=None)
         f1 = f1_score(true_labels, predicted_labels, labels=label_indexes, average=None)
 
-        print(f"Total accuracy: {total_accuracy}\n")
+        print(f'Total accuracy: {total_accuracy}\n')
 
         for i in range(len(class_names)):
             class_name = class_names[i]
 
-            print(f"Metrics for '{class_name}' class:")
-            print(f"Precision: {precision[i]}")
-            print(f"Recall: {recall[i]}")
-            print(f"F1 Score: {f1[i]}\n")
+            print(f'Metrics for "{class_name}" class:')
+            print(f'Precision: {precision[i]}')
+            print(f'Recall: {recall[i]}')
+            print(f'F1 Score: {f1[i]}\n')
 
         matrix = confusion_matrix(true_labels, predicted_labels)
         disp = ConfusionMatrixDisplay(matrix, display_labels=class_names)
         disp.plot()
         plt.show()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
